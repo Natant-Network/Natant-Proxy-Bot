01d068fb146a9dba00d0e25863b8967e30a32050
MotorTruck1221 <tuckerj0606@icloud.com>
2022-11-26 01:52:23 -0700

    BETA v1.0.0 is now OUT

---
+++ commands/list.js
@@ -0,0 +1,30 @@
+const { Message, SlashCommandBuilder, GuildDefaultMessageNotifications, InteractionWebhook, DMChannel, EmbedBuilder  } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('list')
+        .setDescription('List all of the proxies that are available'),
+    async execute(interaction) {
+        let embed = new EmbedBuilder();
+        embed.setTitle('List');
+        //embed.setDescription('This command is currently under development');
+        embed.addFieldset({
+            name: 'Under Development',
+            value: 'This command is currently under development',
+            inline: false
+        },
+        {
+            name: 'Want to donate?',
+            value: 'You can donate to help support the development of this bot',
+            inline: false
+        },
+        {
+            name: 'To Donate, join the server and open a ticket',
+            value: 'https://dsc.gg/natantnetwork',
+            inline: false
+        } 
+        );
+        embed.setColor('#ff0000');
+        await interaction.reply({ embeds: [embed], ephemeral: true});
+    },
+};
