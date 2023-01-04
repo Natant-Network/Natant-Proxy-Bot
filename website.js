5b674d517bfcca19b8e6b6d2f4872d7587c5220b
MotorTruck1221 <tuckerj0606@icloud.com>
2023-01-03 23:46:50 -0700

    YEET

--- website.js
+++ website.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 let { port } = require('./config.json');
-
+//no longer used
 app.use(express.static('website'))
 app.listen(port);
 console.log('listening on port ' + port);
